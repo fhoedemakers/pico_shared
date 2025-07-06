1853d3e099c9bf8f8b349191ad7ba8703d1de1b0
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-07-06 11:07:05 +0200

    Update PSRAM chip select pin configuration for HW_CONFIG 5 and add default setting for RP2350 boards

--- BoardConfigs.cmake
+++ BoardConfigs.cmake
@@ -199,11 +199,11 @@ elseif ( HW_CONFIG EQUAL 5 )
     set(PICO_AUDIO_I2S_CLOCK_PINS_SWAPPED 0 CACHE STRING "Set to 1 if the I2S clock pins are swapped")
     set(BOARD "adafruit_metro_rp2350")         # found in $PICO__SDK_PATH/lib/tinyusb/hw/bsp/rp2040/boards/adafruit_metro_rp2350/board.h
     set(PICO_BOARD "adafruit_metro_rp2350")    # found in $PICO__SDK_PATH/lib/tinyusb/hw/bsp/rp2040/boards/adafruit_metro_rp2350/adafruit_metro_rp2350.h
-    set(ENABLE_PIO_USB 1  CACHE BOOL "Enable PIO USB support")
+    #set(ENABLE_PIO_USB 1  CACHE BOOL "Enable PIO USB support")
     set(PIO_USB_USE_PIO 1 CACHE BOOL "Select the PIO used for PIO-USB")
     set(PIO_DP_PLUS_PIN -1 CACHE STRING "PIO USB DP pin.")
     set(NEEDS_LATEST_TINYUSB_SDK 1 CACHE BOOL "Set to 1 if the latest TinyUSB SDK is needed for this board")
-    set(PSRAM_CS_PIN 47 CACHE STRING "Select the GPIO pin for PSRAM chip select")
+    
 elseif ( HW_CONFIG EQUAL 6 )
     # --------------------------------------------------------------------
 	# RP2040/RP2350 Tiny With PCB
@@ -238,9 +238,6 @@ elseif ( HW_CONFIG EQUAL 6 )
     set(PIO_DP_PLUS_PIN -1 CACHE STRING "PIO USB DP pin.")
 endif ( )
 
-if (NOT DEFINED PSRAM_CS_PIN)
-    set(PSRAM_CS_PIN 0 CACHE STRING "Select the GPIO pin for PSRAM chip select")
-endif()
 if (NOT DEFINED ENABLE_PIO_USB ) 
     set(ENABLE_PIO_USB 0 CACHE BOOL "Enable PIO USB support")
 endif()
@@ -264,6 +261,14 @@ This is needed for supporting the Adafruit Metro RP2350 or when PIO USB is enabl
                ]]
                )
     endif()
+endif()
+# Default PSRAM chip select pin. (RP2350 only)
+# The emulator will try to detect if PSRAM is present. If so it will use it for loading roms.
+# otherwise it will use flash memory to load roms.
+if ( NOT DEFINED PSRAM_CS_PIN )
+    # Pimoroni Pico Plus 2
+    # Adafruit Metro RP2350
+    set(PSRAM_CS_PIN 47 CACHE STRING "Select the GPIO pin for PSRAM chip select")
 endif()
 # --------------------------------------------------------------------
 message("Hardware configuration: ${HW_CONFIG}")
