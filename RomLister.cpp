6725046a4e67ff6cc9a6d9b74e55795129d47bcd
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-12-13 10:05:34 +0100

    Add "SAVESTATES" to ignored directory list in RomLister

--- RomLister.cpp
+++ RomLister.cpp
@@ -113,7 +113,8 @@ namespace Frens
 					strcasecmp(pFile->fname, "System Volume Information") == 0 ||
 					strcasecmp(pFile->fname, "SAVES") == 0 ||
 					strcasecmp(pFile->fname, "EDFC") == 0 ||
-					strcasecmp(pFile->fname, "Metadata") == 0)
+					strcasecmp(pFile->fname, "Metadata") == 0 ||
+					strcasecmp(pFile->fname, "SAVESTATES") == 0)
 				{
 					continue;
 				}
