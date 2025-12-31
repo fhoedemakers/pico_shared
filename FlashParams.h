b082433568d13e4f89e78fc9c0ad479624f73fea
Frank Hoedemakers <frank.hoedemakers@gmail.com>
2025-12-31 17:19:38 +0100

    Refactor FlashParams.h to correctly define maximum frequency and voltage settings for Genesis

--- FlashParams.h
+++ FlashParams.h
@@ -5,9 +5,16 @@
 #define FLASHPARAM_ADDRESS (((uintptr_t)&__flash_binary_end + 0xFFF) & ~0xFFF)
 #define FLASHPARAM_MAGIC "FRENS01"
 #define FLASHPARAM_MIN_FREQ_KHZ 252000 // NES, GB, SMS
-#define FLASHPARAM_MAX_FREQ_KHZ 324000 // Genesis
 #define FLASHPARAM_MIN_VOLTAGE vreg_voltage::VREG_VOLTAGE_1_20
+
+// Genesis max settings
+#if !HSTX
+#define FLASHPARAM_MAX_FREQ_KHZ 324000 
 #define FLASHPARAM_MAX_VOLTAGE vreg_voltage::VREG_VOLTAGE_1_30
+#else
+#define FLASHPARAM_MAX_FREQ_KHZ 378000 // 
+#define FLASHPARAM_MAX_VOLTAGE vreg_voltage::VREG_VOLTAGE_1_60
+#endif
 namespace Frens {
     
     typedef struct 
